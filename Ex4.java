1b8b286262dc194feccbf0d11ce722ca54b67a1d
NavyaSreeKanakala <navyasreesiri999@gmail.com>
2016-11-02 10:03:44 -0700

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -2,22 +2,30 @@ package niit;
 
 public class Ex4 {
 	
-	    String s1="java",s2="programming",s3="";
-	    int l=s1.length(),m=s2.length(),i,j;
-	    public String conca(String s1,String s2){
-	        for(i=0;i<=l-1;i++){
-	            s3+=s1.charAt(i);
-	        }
-	        for(j=0;j<=m-1;j++){
-	            s3+=s2.charAt(j);
-	        }
-	        System.out.println(s3);
-	return s3;
-	    }
-	    public static void main(String[] args) {
-
-	        Ex4 obj1=new Ex4();
-	        obj1.conca("java","programming");
+	    public static void main(String args[]){  
+		String s1="navya";  
+		String s2="kumar";  
+		String s3="navya";
+		String s4="gooDmoRNIng";
+		
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));  
+		
+		System.out.println(s1.substring(2));
+		System.out.println(s2.substring(2,4));
+		
+		s4=s4.toLowerCase();  
+		System.out.println(s4);  
+		
+		s2=s2.toUpperCase();
+		System.out.println(s2);
+		
+		String s5="  big data   ";  
+		System.out.println(s5+"by niit");  
+		System.out.println(s5.trim()+"by niit");
+		
+		System.out.println(s1.contains("vya"));
 
 	}
 	}
