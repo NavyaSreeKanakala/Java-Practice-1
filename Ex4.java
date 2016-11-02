c5734fc41f940b2ee1a06c0c13880c4d344683a2
NavyaSreeKanakala <navyasreesiri999@gmail.com>
2016-11-02 10:10:55 -0700

    Add files via upload

--- Ex4.java
+++ Ex4.java
@@ -1,32 +1,21 @@
 package niit;
 
 public class Ex4 {
+	public static void main(String args[]){
+	StringBuffer s=new StringBuffer("Hello ");  
+	s.append("world"); 
+	System.out.println(s); 
 	
-	    public static void main(String args[]){  
-		String s1="navya";  
-		String s2="kumar";  
-		String s3="navya";
-		String s4="gooDmoRNIng";
-		
-		
-		System.out.println(s1.equals(s2));
-		System.out.println(s1.equals(s3));  
-		
-		System.out.println(s1.substring(2));
-		System.out.println(s2.substring(2,4));
-		
-		s4=s4.toLowerCase();  
-		System.out.println(s4);  
-		
-		s2=s2.toUpperCase();
-		System.out.println(s2);
-		
-		String s5="  big data   ";  
-		System.out.println(s5+"by niit");  
-		System.out.println(s5.trim()+"by niit");
-		
-		System.out.println(s1.contains("vya"));
-
-	}
-	}
-
+	StringBuilder s1=new StringBuilder("Hello ");  
+	s1.append("world"); 
+	System.out.println(s1); 
+	
+	
+	String s2="hi";  
+	s2.concat("this is navya");  
+	System.out.println(s2);  
+	s2=s2.concat("hi....this is navya");  
+	System.out.println(s2);
+	
+}
+}
